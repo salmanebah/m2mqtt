a941681785c31aac8fd14d176ad6f1a53bd1dc77
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2015-09-05 12:23:58 +0200

    Add IMqttClientPersistence interface

---
+++ M2Mqtt/Persistence/IMqttClientPersistence.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace uPLibrary.Networking.M2Mqtt.Persistence
+{
+    public interface IMqttClientPersistence
+    {
+        void Add(string key, MqttMsgContext ctx);
+        void Remove(string key);
+        ICollection GetAll();
+        void Clear();
+    }
+}
