0520ccfb88ebd499a5bd7bf7c810bdbc16823797
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2015-09-05 12:24:37 +0200

    Add MqttMemoryPersistence

---
+++ M2Mqtt/Persistence/MqttMemoryPersistence.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace uPLibrary.Networking.M2Mqtt.Persistence
+{
+    public class MqttMemoryPersistence : IMqttClientPersistence
+    {
+        private Hashtable inflightMessages;
+
+        public MqttMemoryPersistence()
+        {
+            inflightMessages = new Hashtable();
+        }
+        public void Add(string key, MqttMsgContext ctx)
+        {
+            inflightMessages.Add(key, ctx);
+        }
+
+        public  void Remove(string key)
+        {
+            inflightMessages.Remove(key);
+        }
+
+        public ICollection GetAll()
+        {
+            return inflightMessages.Values;
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
