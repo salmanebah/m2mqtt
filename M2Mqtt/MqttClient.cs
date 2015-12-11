8aa958f04d562ff864c439eeb85a3064ea8dbcd0
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2015-12-11 13:10:00 +0100

    Update MqttClient

--- M2Mqtt/MqttClient.cs
+++ M2Mqtt/MqttClient.cs
@@ -1,6 +1,6 @@
 /*
 Copyright (c) 2013, 2014 Paolo Patierno
-Copyright (c) 2015 Salmane Bah
+
 All rights reserved. This program and the accompanying materials
 are made available under the terms of the Eclipse Public License v1.0
 and Eclipse Distribution License v1.0 which accompany this distribution. 
